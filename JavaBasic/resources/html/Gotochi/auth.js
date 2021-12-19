1442d076a9049d53f48f1f621b346025afaa6125
ZenryokuService <tak4545@outlook.jp>
2021-12-19 20:32:26 +0900

    Add Gotochi page

---
+++ JavaBasic/resources/html/Gotochi/auth.js
@@ -0,0 +1,10 @@
+// The client ID is obtained from the Google API Console
+// at https://console.developers.google.com/.
+// If you run this code from a server other than http://localhost,
+// you need to register your own client ID.
+var CLIENT_ID = '221464566685-gimvi3m5iuo06pnqafs4hdor52gge87n.apps.googleusercontent.com';
+var API_KEY = 'AIzaSyBCsw_3PlafILbStvgBz7MpXGAzSw3hv90';
+var OAUTH2_SCOPES = [
+  'https://www.googleapis.com/auth/youtube'
+];
+
